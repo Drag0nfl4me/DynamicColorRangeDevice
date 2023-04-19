5fb7ed0d644e2082df8fa448105f2f7586dad75a
Drag0nfl4me <cgamache1@cnm.edu>
2023-04-19 09:00:09 -0600

    Removed all un-used variables and libraries. This code is so tiny now

--- DynamicColorRange_Program/src/DynamicColorRange_Program.cpp
+++ DynamicColorRange_Program/src/DynamicColorRange_Program.cpp
@@ -6,42 +6,36 @@
 #line 1 "c:/Users/cgame/Documents/IoT/DynamicColorRangeDevice/DynamicColorRange_Program/src/DynamicColorRange_Program.ino"
 /*
  * Project DynamicColorRange_Program
- * Description: A new device that takes two pictures and compares the color change between every pixel. It then creates a new image with the data.
+ * Description: Sends a signal to an ESP32 and flashes a colored LED which outputs two different pictures. 
  * Author: Caden Gamache
- * Date: 04/02/2023 - 
+ * Date: 04/02/2023 - 04/19/2023
  */
-#include "credentials.h"
-
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
-#include "Adafruit_MQTT/Adafruit_MQTT.h"
-
-/****************** Global State ******************/ 
 void setup();
 void loop();
-void turnLed(int color, float brightnessPercent, int redPin, int greenPin, int bluePin);
-#line 14 "c:/Users/cgame/Documents/IoT/DynamicColorRangeDevice/DynamicColorRange_Program/src/DynamicColorRange_Program.ino"
-TCPClient TheClient;
-Adafruit_MQTT_SPARK mqtt(&TheClient,AIO_SERVER,AIO_SERVERPORT,AIO_USERNAME,AIO_KEY); 
-
-/********************* Feeds *********************/  
-Adafruit_MQTT_Subscribe sub_image1 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/image"); 
-//Adafruit_MQTT_Subscribe sub_image2 = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/image2"); 
-
+#line 7 "c:/Users/cgame/Documents/IoT/DynamicColorRangeDevice/DynamicColorRange_Program/src/DynamicColorRange_Program.ino"
+SYSTEM_MODE(SEMI_AUTOMATIC);
 
 /***************************** Declare Variables *****************************/
-int i, x, j;
+// General for loop variables
+int i, j;
 
-const int REDPIN = A5, GREENPIN = A4, BLUEPIN = A3, BUTTONPIN = A2;
+// Pins
+const int REDPIN = A5, GREENPIN = A4, BLUEPIN = A3, BUTTONPIN = A2, CAMERAPIN = A0, ARGONLEDPIN = D7;
+
+// Color declaration and array storage
 const int BLACK = 0, RED = 1, ORANGE = 2, YELLOW = 3, GREEN = 4, CYAN = 5, BLUE = 6, PURPLE = 7, WHITE = 8;
 const int COLOR[] {BLACK, RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE};
-float BRIGHTNESS = 10.0;
-int currentColor;
+int currentColor = 1;
 
-int timer;
-bool buttonPressed, snapPicture, test;
+// Brightness (Percent)
+float BRIGHTNESS = 100.0;
 
-SYSTEM_MODE(SEMI_AUTOMATIC);
+// Button setup
+bool buttonPressed, snapPicture, repeatBlock;
+
+/****************************** Create Prototypes *****************************/
+// Takes in a chosen color (8 total) and brightness and sends the proper voltage to the three pins of a 3W LED to get the desired color
+void turnLed(int color, float brightnessPercent, int redPin, int greenPin, int bluePin);
 
 void setup() {
   Serial.begin(9600);
@@ -51,139 +45,117 @@ void setup() {
   pinMode(GREENPIN,OUTPUT);
   pinMode(BLUEPIN,OUTPUT);
   pinMode(BUTTONPIN,INPUT);
-  pinMode(D7, OUTPUT);
-  pinMode(A0, OUTPUT);
-
-  timer = millis();
+  pinMode(ARGONLEDPIN, OUTPUT);
+  pinMode(CAMERAPIN, OUTPUT);
 }
 
 void loop() {
   buttonPressed = digitalRead(A2);
+  // Button toggle logic
   if(buttonPressed != snapPicture) {
-      if(snapPicture) {
-        snapPicture = !snapPicture;
-      }
-      snapPicture = buttonPressed;
+    if(snapPicture) {
+      snapPicture = !snapPicture;
+    }
+    snapPicture = buttonPressed;
   }
   if (snapPicture) {
-    if (snapPicture != test) {
+    if (snapPicture != repeatBlock) { // This repeatBlock, if statement ensures the code won't repeated with every loop
       // First Picture
-      digitalWrite(D7,HIGH);
-      digitalWrite(A0,HIGH);
+      turnLed(BLACK, BRIGHTNESS, REDPIN, GREENPIN, BLUEPIN);
+      digitalWrite(ARGONLEDPIN,HIGH);
+      digitalWrite(CAMERAPIN,HIGH);  // Sends a high output to ESP32 which it will read and take two pictures
 
       delay(1500);
       digitalWrite(D7,LOW);
-      digitalWrite(A0,LOW);
+      digitalWrite(CAMERAPIN,LOW);   // Stops sending the "take picture" signal
 
-      // Second Picture
+      // Second Picture (Just visual, timed with ESP32's second picture code)
+      turnLed(COLOR[currentColor], BRIGHTNESS, REDPIN, GREENPIN, BLUEPIN);
       delay(1500);
-      digitalWrite(D7,HIGH);
-      digitalWrite(A0,HIGH);
+      digitalWrite(ARGONLEDPIN,HIGH);
 
       delay(1500);
-      digitalWrite(D7,LOW);
-      digitalWrite(A0,LOW);
+      digitalWrite(ARGONLEDPIN,LOW);
+      turnLed(BLACK, BRIGHTNESS, REDPIN, GREENPIN, BLUEPIN);
 
-      test = 1;
+      repeatBlock = 1;  // Sets repeatBlock to true
+      // Cycles through a color array with ever call
+      currentColor++;
+      if(currentColor>8) {
+        currentColor = 1;
+      }
     }
-    //String parse0 = Serial1.readStringUntil('%X',0xFFD9);
-    //Serial.printf("%s",parse0.c_str());
   }
+  // Sets everything to an idle state
   if (snapPicture == 0) {
-    if (snapPicture != test) {
-      digitalWrite(D7,LOW);
-      digitalWrite(A0,LOW);
+    if (snapPicture != repeatBlock) {
+      digitalWrite(ARGONLEDPIN,LOW);
+      digitalWrite(CAMERAPIN,LOW);
 
-      test = 0;
-    }
-  }
-
-  // /*********************** Subscriptions ***********************/
-  // Adafruit_MQTT_Subscribe *subscription;
-  // while ((subscription = mqtt.readSubscription(100))) {
-  //   if (subscription == &sub_image1) {
-  //     //image1 = atof((char *)sub_image1.lastread);
-  //   }
-  // }
-
-
-  if((millis()-timer) > 5000) {
-    timer = millis();
-    currentColor++;
-    turnLed(COLOR[currentColor], BRIGHTNESS, REDPIN, GREENPIN, BLUEPIN);
-    if (currentColor>8) {
-      currentColor = 0;
+      repeatBlock = 0;
     }
   }
 }
 
 void turnLed(int color, float brightnessPercent, int redPin, int greenPin, int bluePin) {
-    static int black = 0, red = 1, orange = 2, yellow = 3, green = 4, cyan = 5, blue = 6, purple = 7, white = 8;
-    // quick reset, otherwise colors mix ever so often
+  static int black = 0, red = 1, orange = 2, yellow = 3, green = 4, cyan = 5, blue = 6, purple = 7, white = 8;
+  // Quick reset, otherwise colors mix ever so often
+  analogWrite(redPin, LOW);
+  analogWrite(greenPin, LOW);
+  analogWrite(bluePin, LOW);
+
+  if(color == black) {
     analogWrite(redPin, LOW);
     analogWrite(greenPin, LOW);
     analogWrite(bluePin, LOW);
-
-    if(color == black) {
-        analogWrite(redPin, LOW);
-        analogWrite(greenPin, LOW);
-        analogWrite(bluePin, LOW);
-        //Serial.printf("Current LED color = black     \r");
-    }
-    if(color == red) {
-        analogWrite(redPin, 255.0/(101.0-brightnessPercent));
-        analogWrite(greenPin, LOW);
-        analogWrite(bluePin, LOW);
-        //Serial.printf("Current LED color = red       \r");
-    }
-    if(color == orange) {
-        analogWrite(redPin, 235.0/(101.0-brightnessPercent));
-        analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
-        analogWrite(bluePin, LOW);
-        //Serial.printf("Current LED color = orange     \r");
-    }
-    if(color == yellow) {
-        analogWrite(redPin, 200.0/(101.0-brightnessPercent));
-        analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
-        analogWrite(bluePin, LOW);
-        //Serial.printf("Current LED color = yellow     \r");
-    }
-    if(color == green) {
-        analogWrite(redPin, LOW);
-        analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
-        analogWrite(bluePin, LOW);
-        //Serial.printf("Current LED color = green     \r");
-    }
-    if(color == cyan) {
-        analogWrite(redPin, LOW);
-        analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
-        analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
-        //Serial.printf("Current LED color = cyan     \r");
-    }
-    if(color == blue) {
-        analogWrite(redPin, LOW);
-        analogWrite(greenPin, LOW);
-        analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
-        //Serial.printf("Current LED color = blue     \r");
-    }
-    if(color == purple) {
-        analogWrite(redPin, 15.0/(101-brightnessPercent));
-        analogWrite(greenPin, LOW);
-        analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
-        //Serial.printf("Current LED color = purple     \r");
-    }
-    if(color == white) {
-        analogWrite(redPin, 145.0/(101.0-brightnessPercent));
-        analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
-        analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
-        //Serial.printf("Current LED color = white     \r");
-    }
+    //Serial.printf("Current LED color = black     \r");
+  }
+  if(color == red) {
+    analogWrite(redPin, 255.0/(101.0-brightnessPercent));
+    analogWrite(greenPin, LOW);
+    analogWrite(bluePin, LOW);
+    //Serial.printf("Current LED color = red       \r");
+  }
+  if(color == orange) {
+    analogWrite(redPin, 235.0/(101.0-brightnessPercent));
+    analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
+    analogWrite(bluePin, LOW);
+    //Serial.printf("Current LED color = orange     \r");
+  }
+  if(color == yellow) {
+    analogWrite(redPin, 200.0/(101.0-brightnessPercent));
+    analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
+    analogWrite(bluePin, LOW);
+    //Serial.printf("Current LED color = yellow     \r");
+  }
+  if(color == green) {
+    analogWrite(redPin, LOW);
+    analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
+    analogWrite(bluePin, LOW);
+    //Serial.printf("Current LED color = green     \r");
+  }
+  if(color == cyan) {
+    analogWrite(redPin, LOW);
+    analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
+    analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
+    //Serial.printf("Current LED color = cyan     \r");
+  }
+  if(color == blue) {
+    analogWrite(redPin, LOW);
+    analogWrite(greenPin, LOW);
+    analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
+    //Serial.printf("Current LED color = blue     \r");
+  }
+  if(color == purple) {
+    analogWrite(redPin, 15.0/(101-brightnessPercent));
+    analogWrite(greenPin, LOW);
+    analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
+    //Serial.printf("Current LED color = purple     \r");
+  }
+  if(color == white) {
+    analogWrite(redPin, 145.0/(101.0-brightnessPercent));
+    analogWrite(greenPin, 255.0/(101.0-brightnessPercent));
+    analogWrite(bluePin, 255.0/(101.0-brightnessPercent));
+    //Serial.printf("Current LED color = white     \r");
+  }
 }
-
-// void colorChange(int *array[][]) {
-
-// }
-
-// void colorChangeRGB(int *array[][]) {
-
-// }
